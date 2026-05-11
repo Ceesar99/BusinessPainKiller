6f549c25656a76304ef0554d839dae7d2e0aa6ae
Ceesar99 <dishusamuelprosper@gmail.com>
2026-05-11 22:04:52 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import Dashboard from './components/Dashboard'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  )
+}
